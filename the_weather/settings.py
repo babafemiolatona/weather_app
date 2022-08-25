7cdd6ee027b05be5a4f448ff9279535d2fc7c9b5
hgfemi <femiolatona@gmail.com>
2022-08-25 16:18:45 +0200

    Ready for deployment to heroku.

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -20,10 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-import os
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-=98!vn5etbq2m$@lzsmcx1^5nmi!e_-ki_mxxv%jv$$5e2al9='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
