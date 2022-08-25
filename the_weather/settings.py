64a20196f09c628da840a829a022d78e3eec96af
hgfemi <femiolatona@gmail.com>
2022-08-25 16:38:26 +0200

    First commit

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=98!vn5etbq2m$@lzsmcx1^5nmi!e_-ki_mxxv%jv$$5e2al9='
+
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
